2da9a373f90540a17fe449aa2f9b82ad0f4bef83
Raffaele FIccadenti <r.ficcadenti@exolab.app>
2024-01-03 12:04:22 +0100

    Tipi valore e riferimento

---
+++ 07 - Metodi/Metodi/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Metodi;
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Metodi !!!");
+        Console.WriteLine($"{Program.somma(10,20)}");
+        Console.WriteLine($"{Program.somma(10.1d,20.1d)}");
+        Console.Write("Clcola il fattoriale di: ");
+        int n=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Il fattoriale di {n} è {Program.fattoriale(n)}");
+
+    }
+
+    static int somma(int a,int b)
+    {
+        int s=a+b;
+        return s;
+    }
+
+    static double somma(double a,double b)
+    {
+        double s=a+b;
+        return s;
+    }
+
+    static int fattoriale(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }else{
+            return n*fattoriale(n-1);
+        }
+    }
+
+}
---
+++ 08 - Valore e Riferimento/ValoreRiferimento/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ValoreRiferimento;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Valore e riferimento!");
+
+        //tipi valore memorizzato nello stack
+        int val=120;
+
+        //tipi riferimento memorizzato nello heap il contenuto "Ciao" e nello stack il riferimento s,s1,arr
+        string s="Ciao"; 
+        string[] s1= new []{"primo","secondo"};
+        int[] arr=new [] {10,20};
+
+        //semantica tipi valore
+        int a=100;
+        int b=a;
+        a=50;
+        Console.WriteLine($"a={a}, b={b}");
+
+        //semantica tipi riferimento
+        int[] arr1=new []{5,10};
+        int[] arr2=arr1;
+
+        arr2[0]=100;
+
+        foreach(int v in arr1)
+        {
+            Console.WriteLine($"{v}");
+        }
+
+        foreach(int v in arr2)
+        {
+            Console.WriteLine($"{v}");
+        }
+
+
+
+        
+    }
+}
