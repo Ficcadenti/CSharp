7eba3cfec6334c69cfaf4dba12edfc183e1e8776
Raffaele FIccadenti <r.ficcadenti@exolab.app>
2024-01-15 18:43:24 +0100

    Avanzamento delegate action & func

---
+++ 19 - Delegate/Delegate/Esercitazione.cs
@@ -0,0 +1,41 @@
+﻿namespace Esercitazione{
+
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I Delegate !!!");
+            int x=-12;
+            Controlla(x);
+            int[] arr=new int[9];
+            for(int i=0;i<9;i++)
+            {
+                arr[i]=i;
+            }
+
+            invertiArry(arr);
+            
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        static void Controlla(int x)
+        {
+            if((x&0x1)==1)
+                Console.WriteLine("Dispari");
+            else
+                Console.WriteLine("Pari");
+        }
+
+        static void invertiArry(int[] array) {
+            for (int i = 0; i < array.Length / 2; i++) {
+                (array[array.Length - i - 1], array[i]) = (array[i], array[array.Length - i - 1]);
+            }
+        }
+    }
+}
--- 19 - Delegate/Delegate/Program.cs
+++ 19 - Delegate/Delegate/Program.cs
@@ -1,12 +1,130 @@
 ﻿namespace Delegate{
 
-    public delegate bool Confronta(int x,int y);
+    public delegate bool ConfrontoDelegate(int x,int y);
+    public delegate void MC_ConfrontoDelegate(int x,int y);
+
+    public delegate bool ConfrontoDelegate<T>(T x,T y);
+
+    public delegate void Action<T1,T2>(T1 arg1,T2 arg2);
+
+    public delegate TRet Func<T1,T2, TRet>(T1 arg1,T2 arg2);
 
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("I Delegate !!!");
+            
+            Program p=new Program();
+            //notazione complessa
+            ConfrontoDelegate cd=new ConfrontoDelegate(p.MaggioreDi);
+            //notazione compatta
+            //ConfrontoDelegate cd=p.MaggioreDi;
+
+            Console.WriteLine("MaggiorDi");
+            Console.WriteLine(cd(10,4));
+            Console.WriteLine(cd(10,40));
+
+            cd=p.MinoreDi;
+            Console.WriteLine("MinoreDi");
+            Console.WriteLine(cd(10,4));
+            Console.WriteLine(cd(10,40));
+
+
+            //chiamata alternativa al delegate
+            cd=p.UgualeA;
+            Console.WriteLine("UgualeA");
+            Console.WriteLine(cd.Invoke(10,10));
+            Console.WriteLine(cd.Invoke(10,20));
+
+            //delegate multicast
+            MC_ConfrontoDelegate cd_mc=p.MC_MaggioreDi;
+            cd_mc+=p.MC_MinoreDi;
+            cd_mc+=p.MC_UgualeA;
+
+            cd_mc(10,4);
+            cd_mc(10,40);
+            cd_mc(10,10);
+
+            //delegate generics
+            ConfrontoDelegate<int> cd1_g=p.MaggioreDi_G;
+            ConfrontoDelegate<double> cd2_g=p.MaggioreDi_G;
+
+            Console.WriteLine(cd1_g(10,4));
+            Console.WriteLine(cd2_g(3.4d,2.3d));
+
+            //delegate action
+            Action<int,string> MyAction=p.MyFunc;
+            MyAction(10,"Gabriele");
+
+             //delegate function
+            Func<int,string,String> Func=p.MyFuncRet;
+            Console.WriteLine(Func(10,"Gabriele"));
+
         }
+
+        //delegate
+        public bool MaggioreDi(int x,int y)
+        {
+            Console.WriteLine($"x={x} > y={y} ?");
+            return (x>y)?true:false;
+        }
+
+        public bool MinoreDi(int x,int y)
+        {
+            Console.WriteLine($"x={x} < y={y} ?");
+            return (x<y)?true:false;
+        }
+
+        public bool UgualeA(int x,int y)
+        {
+            Console.WriteLine($"x={x} == y={y} ?");
+            return (x==y)?true:false;
+        }
+
+        //delegate multicast
+        public void MC_MaggioreDi(int x,int y)
+        {
+            bool b=(x>y)?true:false;
+            Console.WriteLine($"x={x} > y={y} ? {b}");
+        }
+
+        public void MC_MinoreDi(int x,int y)
+        {
+            bool b=(x<y)?true:false;
+            Console.WriteLine($"x={x} < y={y} ? {b}");
+        }
+
+        public void MC_UgualeA(int x,int y)
+        {
+            bool b=(x==y)?true:false;
+            Console.WriteLine($"x={x} == y={y} ? {b}");
+        }
+
+        //delegate generics
+        public bool MaggioreDi_G(int x,int y)
+        {
+            Console.WriteLine($"(int) x={x} > y={y} ?");
+            return (x>y)?true:false;
+        }
+
+        public bool MaggioreDi_G(double x,double y)
+        {
+            Console.WriteLine($"(double) x={x} > y={y} ?");
+            return (x>y)?true:false;
+        }
+
+        //delegate action
+        void MyFunc(int x,string y)
+        {
+             Console.WriteLine($"Action => x={x};y={y}");
+        }
+
+        //delegate action
+        string MyFuncRet(int x,string y)
+        {
+             return $"Func => x={x};y={y}";
+        }
+
     }
 }
