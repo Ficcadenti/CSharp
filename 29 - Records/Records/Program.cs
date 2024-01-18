aeecdac10b87f98126a2c638ff43516969b5a7f5
Raffaele FIccadenti <r.ficcadenti@exolab.app>
2024-01-18 13:52:49 +0100

    I Records

--- 29 - Records/Records/Program.cs
+++ 29 - Records/Records/Program.cs
@@ -9,14 +9,21 @@
             Libro l1=new Libro("Aspetta primavera, Bandini","John Fante");
             Libro l2=new Libro("Bravo, Burro!","John Fante");
             Libro l3=new Libro("Bravo, Burro!","John Fante");
+            Libro1 l4=new Libro1("Bravo, Burro!","John Fante");
+            Libro1 l5=new Libro1("Bravo, Burro!","John Fante");
 
             Console.WriteLine(l1);
             Console.WriteLine(l2);
             Console.WriteLine(l3);
+            Console.WriteLine(l4);
 
             Console.WriteLine(l1==l2);
-            Console.WriteLine(l3==l3);
+            Console.WriteLine(l2==l3);
+            Console.WriteLine(l5==l4);
 
+            //l5.Titolo="Chiedi alla polvere";
+            Console.WriteLine($"Titolo={l5.Titolo} Autore={l5.Autore}");
+            
 
         }
     }
@@ -33,13 +40,8 @@
             this.Titolo=titolo; 
             this.Autore=autore; 
         }
-
-        /*
-        public override string ToString()
-        {
-            return $"{base.ToString} - {Titolo} - {Autore}";
-        }
-        */
     }
+
+    public record Libro1(string Titolo, string Autore);
 
 }
