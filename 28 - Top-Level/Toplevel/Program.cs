a84e9171efb4f74fd4cbbce3e42a5a281352ef01
Raffaele FIccadenti <r.ficcadenti@exolab.app>
2024-01-18 11:58:28 +0100

    Top Level statements

--- 28 - Top-Level/Toplevel/Program.cs
+++ 28 - Top-Level/Toplevel/Program.cs
@@ -1,6 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Diagnostics.Contracts;
+
 Console.WriteLine("Top-Level statements !!!");
 foreach(string a in args)
 {
     Console.WriteLine($"Arg: {a}");
+}
+
+var instance=new MyClass{
+    MyProp="Raffaele"
+};
+//instance.MyProp="Sofia"; //ERRORE
+instance.show();
+
+
+public class MyClass
+{
+    public string? MyProp {get;init;}
+
+    public void show()
+    {
+        Console.WriteLine($"MyProp={this.MyProp}");
+    }
 }
