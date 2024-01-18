cba3d4d58b2c130f866e51c027590a6737cc5981
Raffaele FIccadenti <r.ficcadenti@exolab.app>
2024-01-18 11:47:18 +0100

    Top Level statements

---
+++ 27 - Classe Object/Object/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Object{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("La classe Object !!!");
+
+            AClass a=new AClass();
+            a.aVar=10;
+            Console.WriteLine(a);
+            Console.WriteLine(a.GetHashCode());
+            AClass a1=new AClass();
+            a1.aVar=10;
+            Console.WriteLine(a.Equals(a1));
+
+
+            BClass b=new BClass();
+            b.aVar=5;
+            b.bVar=10;
+            Console.WriteLine(b);
+        }
+    }
+
+    //superclasse o base
+    public class AClass : System.Object //di default o object
+    {
+        public int aVar;
+
+        public override string? ToString()
+        {
+            return $"{base.ToString()} -> aVar{aVar}";
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(aVar);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            #nullable disable
+            if(obj!=null)
+                return aVar==(obj as AClass).aVar;
+            else return false;
+            #nullable enable
+        }
+
+    }
+
+    //classe derivate
+    public class BClass: AClass
+    {
+        public int bVar;
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} -> aVar={aVar} bVar={bVar}";
+        }
+    }
+
+    //classe derivate
+    public class CClass: BClass
+    {
+        public int cVar;
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} -> cVar{cVar} bVar={bVar} aVar={aVar}";
+        }
+    }
+}
---
+++ 28 - Top-Level/Toplevel/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Top-Level statements !!!");
+foreach(string a in args)
+{
+    Console.WriteLine($"Arg: {a}");
+}
