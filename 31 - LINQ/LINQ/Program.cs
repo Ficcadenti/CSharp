0e333a35bf0fe1d66d4220067d1c4d191c3fed12
Raffaele FIccadenti <r.ficcadenti@exolab.app>
2024-01-19 10:46:51 +0100

    Primi passi non LINQ

--- 31 - LINQ/LINQ/Program.cs
+++ 31 - LINQ/LINQ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Collections;
 
 namespace LINQ{
 
@@ -24,6 +25,33 @@ namespace LINQ{
             {
                 Console.WriteLine(s);
             }
+
+            ArrayList coll = new ArrayList(); 
+            coll.Add("Valeria"); 
+            coll.Add("Raffaele"); 
+            coll.Add("Sofia"); 
+            coll.Add("Maria"); 
+            coll.Add("Gatto"); 
+            coll.Add("Gabriele"); 
+            coll.Add("Mia"); 
+
+
+            coll.Sort();
+
+            IEnumerator enumerator = coll.GetEnumerator(); 
+            int cont=0;
+            while (enumerator.MoveNext()) 
+            { 
+                Console.WriteLine($"coll[{cont++}] = {enumerator.Current}");
+            }
+
+            var query = from String s in coll where s.StartsWith("M") orderby s descending select s;
+            foreach(string str in query)
+            {
+                Console.WriteLine(str);
+            }
+            
+
 
         }
     }
